e96efe678f2c819798786e3edaf47be571b883f9
Jehymison Gil Alves Oliveira <jehymes.gil@live.com>
2021-01-11 13:35:26 -0300

    task 0003 - API 1.0.0

--- config/database.js
+++ config/database.js
@@ -5,3 +5,9 @@ module.exports = mongoose.connect('mongodb://localhost/db_finance', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}.'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}.'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}.'"
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'."
