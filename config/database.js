2bcade30ca0f11f862f93da68c5aa2a434b559a0
Jehymison Gil Alves Oliveira <jehymes.gil@live.com>
2021-01-25 19:31:53 -0300

    task 0004 - Cors

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 module.exports = mongoose.connect('mongodb://localhost/db_finance', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
