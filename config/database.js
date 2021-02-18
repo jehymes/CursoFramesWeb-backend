2722b7e3805d680330e698ca33953ed0a2ec7a32
Jehymeson Gil <jehymes.gil@live.com>
2021-02-18 15:57:26 -0300

    backend v-1.0

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/db_finance'
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
