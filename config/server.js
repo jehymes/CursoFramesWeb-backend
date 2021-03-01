785d5d8e29394353faa2000629851af31c179929
Jehymeson Gil <jehymes.gil@live.com>
2021-03-01 15:04:11 -0300

    task 0006

--- config/server.js
+++ config/server.js
@@ -3,15 +3,13 @@ const port = process.env.PORT
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-// const allowCors = require('./cors')
-const cors = require('cors');
+const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: false }))
 server.use(bodyParser.json())
-// server.use(allowCors)
+server.use(allowCors)
 server.use(queryParser())
-server.use(cors());
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`)
