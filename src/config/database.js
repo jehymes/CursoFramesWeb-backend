cdf0cac06879210599367130cada64bf156dd7f8
Jehymeson Gil <jehymes.gil@live.com>
2021-03-01 18:39:10 -0300

    task 0013

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-// const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/db_finance'
-const url = 'mongodb://localhost/db_finance'
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/db_finance'
+// const url = 'mongodb://localhost/db_finance'
 module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
