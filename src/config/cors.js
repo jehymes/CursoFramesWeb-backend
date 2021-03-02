2473d9cc8ad7b472f75d6e521b3fa5a89cc79fc4
Jehymeson Gil <jehymes.gil@live.com>
2021-03-02 15:42:21 -0300

    task 0018

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', 'https://primeirapp.herokuapp.com')
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Max-Age', 86400)
     next()
   }
